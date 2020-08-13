9bf73401e79ec2f0bbef0dac9ad7f945a2906f42
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-08-13 18:01:59 +0300

    Add responsive scripts

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function toggleNavMenu(){
+    var x = document.getElementById("header");
+    if (x.className === "header") {
+      x.className += " responsive";
+      var humburgerBtn = document.getElementById("toggleBtn");
+      humburgerBtn.className += " opened"
+    } else {
+      x.className = "header";
+      console.log("Have not Updated header")
+    }
+}
